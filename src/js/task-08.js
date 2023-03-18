2814b7c7bb4ac36c09b2c1d89bc20bbbbe26f4e9
MishaHryvinskyi <hryvinskyim@gmail.com>
2023-03-18 21:13:33 +0200

    did tasks 8-9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const { email, password } = form.elements;
+
+if (email.value === '' || password.value === '') {
+    alert('All fields must be filled!');
+    return;
+    }
+
+    const formData = { email: email.value, password: password.value };
+    console.log(formData);
+
+    form.reset();
+});
+
+
+
+
+
+
+
+
+
+
 
